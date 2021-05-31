89b04f0a634ef5f72d9ebe5a3797a79c6f559af4
jarekpawlak <jarek.p89@gmail.com>
2021-05-31 12:35:01 +0200

    Added parsing cmd args and displaying banner

--- server/main.py
+++ server/main.py
@@ -1,10 +1,32 @@
 from server import Server
 
+import argparse
 
-def main():
-    server = Server(12345)
+
+def display_banner():
+    banner = """
+      __  _____  ___
+     / / / / _ \/ _ \  ___ ___ _____  _____ ____
+    / /_/ / // / ___/ (_-</ -_) __/ |/ / -_) __/
+    \____/____/_/    /___/\__/_/  |___/\__/_/
+    
+    """
+    print(banner)
+
+
+def main(args):
+    display_banner()
+    server = Server(args.port)
     server.run()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='UDP Server. Connected users may send sentences to each other.')
+    parser.add_argument('--port', type=int, default=12345,
+                        help='Port used by UDP server. Clients should connect to that port.')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
